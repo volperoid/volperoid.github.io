5508fe5c217d63d7eaaa5e2b4832fdfb4cf7b469
volperoid <43482486+volperoid@users.noreply.github.com>
2020-12-13 22:52:16 +0100

    Added comment

--- js/engine.js
+++ js/engine.js
@@ -7,7 +7,7 @@ function clearValue(){
 };
 function calc(){
     let operation = document.getElementById("calc-display").value;
-    if (/^[\d()\/*.+-]+$/.test(operation)) {
+    if (/^[\d()\/*.+-]+$/.test(operation)) { // ! security measure for `eval()`
         document.getElementById("calc-display").value = eval(operation);
     } else {
         alert("Only numbers and mathematical operators allowed.");
