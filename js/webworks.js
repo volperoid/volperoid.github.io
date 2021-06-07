6c1c2106cb96539b484a96c1d26cd6bb18c97564
volperoid <43482486+volperoid@users.noreply.github.com>
2021-06-08 01:43:09 +0200

    fixed accordion

--- js/webworks.js
+++ js/webworks.js
@@ -1,8 +1,14 @@
+
+// * get accordion slides //
+
 const nodeList = document.querySelectorAll('.illustration');
+
+// * functions //
+
+
 function flexIt(target){
-    let selectorString = "#"+target.id+" .wrapper";
-    if (target.style.flex !== 10) {
-        target.style.flex = 10;
+    function closePrevSlide () {
+        const nodeList = document.querySelectorAll('.illustration');
         for (let i = 0; i < nodeList.length; i++) {
             if ('slide'+i !== target.id) {
                 document.querySelector('#slide'+i).style.flex = 1;
@@ -11,25 +17,47 @@ function flexIt(target){
                 document.querySelector('#slide'+i+' '+'.wrapper').style.backgroundColor = 'transparent';
             }
         }
+    }
+    function closeCurrentSlide () {
+        const nodeList = document.querySelectorAll('.illustration');
+        for (let i = 0; i < nodeList.length; i++) {
+            if ('slide'+i == target.id) {
+                document.querySelector('#slide'+i).style.flex = 1;
+                document.querySelector('#slide'+i+' '+'.wrapper').style.transform = 'translateX(-100%)';
+                document.querySelector('#slide'+i+' '+'.wrapper').style.opacity = 0;
+                document.querySelector('#slide'+i+' '+'.wrapper').style.backgroundColor = 'transparent';
+            }
+        }
+    }
+    let selectorString = "#"+target.id+" "+".wrapper";
+    if (target.style.flex !== '10 1 0%') {
+        target.style.flex = '10 1 0%';
         document.querySelector(selectorString).style.transform = 'translateX(0)';
         document.querySelector(selectorString).style.opacity = 1;
         document.querySelector(selectorString).style.backgroundColor = '#ffffffcc';
-    } else {
-        target.style.flex = 1;
+        closePrevSlide();
+    } else if (target.style.flex == '10 1 0%') {
+        target.style.flex = '1 1 0%';
+        closeCurrentSlide();
     }
 }
+
+//------------------------------------------------------------------------------------------------------------------//
+
 function dimIt(target) {
-    target.style.filter = 'sepia()';
+    console.log('hi')
+    target.style.filter = 'sepia(100)';
 }
+
+//------------------------------------------------------------------------------------------------------------------//
+
 function slideIt (targetButton) {
-    //function colorChangeCrimson () {document.querySelector('#visit i').style.color = 'crimson';
-    //console.log('it works');}
-    //function colorChangeWhite () {document.querySelector('#visit i').style.color = 'white';
-    //console.log('it works');}
     if (targetButton == 1) {
         document.querySelector('#toggle').style.transform = 'translateY(0%)';
+
         console.log('Theme: Dark');
-        let nodeList = document.querySelectorAll('.section');
+
+        const nodeList = document.querySelectorAll('.section');
         nodeList.forEach(element => {
             element.style.backgroundColor = '#121212cc';
             element.style.color = 'white';
@@ -37,12 +65,14 @@ function slideIt (targetButton) {
         document.querySelector('#banner').style.backgroundColor = '#121212cc';
         document.querySelector('header').style.color = 'white';
         document.querySelector('#author').style.color = 'hsla(0, 0%, 100%, 0.50)';
-        //document.querySelector('#visit').style.color = 'hsla(0, 0%, 100%, 0.50)';
-        //document.querySelector('#visit').addEventListener('hover', colorChangeWhite);
+        document.querySelector('#visit').style.color = 'hsla(0, 0%, 100%, 0.50)';
+        document.querySelectorAll('.wrapper').style.backgroundColor = '#121212cc';
     } else if (targetButton == 0) {
         document.querySelector('#toggle').style.transform = 'translateY(-50%)';
+
         console.log('Theme: Light');
-        let nodeList = document.querySelectorAll('.section');
+
+        const nodeList = document.querySelectorAll('.section');
         nodeList.forEach(element => {
             element.style.backgroundColor = 'white';
             element.style.color = '#121212';
@@ -50,14 +80,31 @@ function slideIt (targetButton) {
         document.querySelector('#banner').style.backgroundColor = '#ffffffcc';
         document.querySelector('header').style.color = '#121212';
         document.querySelector('#author').style.color = 'hsla(0, 0%, 7%, 0.5)';
-        //document.querySelector('#visit').style.color = 'hsla(0, 0%, 7%, 0.5)';
-        //document.querySelector('#visit').addEventListener('hover', colorChangeCrimson);
+        document.querySelector('#visit').style.color = 'hsla(0, 0%, 7%, 0.5)';
+        document.querySelector('.wrapper').style.backgroundColor = '#ffffffcc';
     }
 }
+
+//------------------------------------------------------------------------------------------------------------------//
+
+function scrollToSection () {
+    console.log('function entered');
+    for (let i = 0; i < 2; i++) {
+        if (i < 1) {
+            document.querySelector('section'+'-'+i).scrollIntoView();
+        } else if (i = 1) {
+            document.querySelector('body').scrollIntoView();
+        };
+
+    }
+}
+
+// * load event listeners //
+
 nodeList.forEach(element => {
     element.addEventListener('click', () => {flexIt(element)});
     element.addEventListener('hover', () => {dimIt(element)});
 });
 document.querySelector('#light').addEventListener('click', () => {slideIt(1)});
 document.querySelector('#dark').addEventListener('click', () => {slideIt(0)});
-
+document.querySelector('#nav-arrow').addEventListener('click', scrollToSection)
