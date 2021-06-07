3a69bf1159ef84b334e378df85dc1c44a9d441bf
volperoid <43482486+volperoid@users.noreply.github.com>
2021-06-07 20:17:26 +0200

    added theme toggle and partial theme functionality

--- js/webworks.js
+++ js/webworks.js
@@ -20,21 +20,44 @@ function flexIt(target){
 }
 function dimIt(target) {
     target.style.filter = 'sepia()';
-
+}
+function slideIt (targetButton) {
+    //function colorChangeCrimson () {document.querySelector('#visit i').style.color = 'crimson';
+    //console.log('it works');}
+    //function colorChangeWhite () {document.querySelector('#visit i').style.color = 'white';
+    //console.log('it works');}
+    if (targetButton == 1) {
+        document.querySelector('#toggle').style.transform = 'translateY(0%)';
+        console.log('Theme: Dark');
+        let nodeList = document.querySelectorAll('.section');
+        nodeList.forEach(element => {
+            element.style.backgroundColor = '#121212cc';
+            element.style.color = 'white';
+        });
+        document.querySelector('#banner').style.backgroundColor = '#121212cc';
+        document.querySelector('header').style.color = 'white';
+        document.querySelector('#author').style.color = 'hsla(0, 0%, 100%, 0.50)';
+        //document.querySelector('#visit').style.color = 'hsla(0, 0%, 100%, 0.50)';
+        //document.querySelector('#visit').addEventListener('hover', colorChangeWhite);
+    } else if (targetButton == 0) {
+        document.querySelector('#toggle').style.transform = 'translateY(-50%)';
+        console.log('Theme: Light');
+        let nodeList = document.querySelectorAll('.section');
+        nodeList.forEach(element => {
+            element.style.backgroundColor = 'white';
+            element.style.color = '#121212';
+        });
+        document.querySelector('#banner').style.backgroundColor = '#ffffffcc';
+        document.querySelector('header').style.color = '#121212';
+        document.querySelector('#author').style.color = 'hsla(0, 0%, 7%, 0.5)';
+        //document.querySelector('#visit').style.color = 'hsla(0, 0%, 7%, 0.5)';
+        //document.querySelector('#visit').addEventListener('hover', colorChangeCrimson);
+    }
 }
 nodeList.forEach(element => {
     element.addEventListener('click', () => {flexIt(element)});
     element.addEventListener('hover', () => {dimIt(element)});
 });
+document.querySelector('#light').addEventListener('click', () => {slideIt(1)});
+document.querySelector('#dark').addEventListener('click', () => {slideIt(0)});
 
-idleState = {
-    'border-right-color': '#484848'
-}
-$('#btn-moon').hover(
-    function() {
-        $('#moon').css({'border-right-color': '#f0e68c'});
-    },
-    function() {
-        $('#moon').css(idleState);
-    }
-);
